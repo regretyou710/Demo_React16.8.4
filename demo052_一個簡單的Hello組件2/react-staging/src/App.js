eef62b76362e39f33d5995ad9fb30857948f15f6
eason <regretyou710@gmail.com>
2022-10-28 18:10:00 +0800

    202210281809

---
+++ demo052_一個簡單的Hello組件2/react-staging/src/App.js
@@ -0,0 +1,29 @@
+//創建"外殼"組件App
+
+// import React from "react";
+
+// class App extends React.Component {
+//   render() {
+//     return <div>hello,react!</div>;
+//   }
+// }
+
+// //暴露App組件
+// export default App;
+
+//進行代碼優化--------------------------------------------
+import React, { Component } from "react";
+import Hello from "./component/Hello";
+import Welcome from "./component/Welcome";
+
+//創建並暴露App組件
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+        <Welcome />
+      </div>
+    );
+  }
+}
