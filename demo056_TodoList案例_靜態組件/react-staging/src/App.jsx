568b36b0ab3fda5c5d8fd670c0dfc73a0deb4288
eason <regretyou710@gmail.com>
2022-10-29 21:23:15 +0800

    202210292123

---
+++ demo056_TodoList案例_靜態組件/react-staging/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Header from "./component/Header";
+import List from "./component/List";
+import Footer from "./component/Footer";
+import "./App.css";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="todo-container">
+        <div className="todo-wrap">
+          <Header />
+          <List />
+          <Footer />
+        </div>
+      </div>
+    );
+  }
+}
