4b8be93fb758a88843f810af044bdcc2dc345e1a
eason <regretyou710@gmail.com>
2023-03-03 20:31:04 +0800

    202303032031

---
+++ demo115_項目打包運行/react-staging/src/redux/reducers/person.js
@@ -0,0 +1,15 @@
+import { ADD_PERSON } from "../constant";
+
+// 初始化人的列表
+const initState = [{ id: "001", name: "tom", age: 18 }];
+
+export default (preState = initState, action) => {
+  const { type, data } = action;
+
+  switch (type) {
+    case ADD_PERSON:
+      return [data, ...preState];
+    default:
+      return preState;
+  }
+};
